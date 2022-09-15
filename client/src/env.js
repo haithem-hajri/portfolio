2c7e4e4a79e8de9d0bb275d867509ec057a32823
haithem711 <49453214+haithem711@users.noreply.github.com>
2022-09-15 06:24:47 +0100

    make it better

--- client/src/env.js
+++ client/src/env.js
@@ -1,7 +1,8 @@
 
 
-
-export const API_URL = "http://localhost:5001/api";
+//https://haithem5.herokuapp.com/
+//http://localhost:5001/api
+export const API_URL = "https://haithem5.herokuapp.com/api";
 
 
 
