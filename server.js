222a342dc5de057a6ad21e63c050b4a72aa1994f
haithem711 <49453214+haithem711@users.noreply.github.com>
2022-12-15 15:00:26 +0100

    fix

--- server.js
+++ server.js
@@ -99,9 +99,9 @@ if (
   process.env.NODE_ENV === "production" ||
   process.env.NODE_ENV === "staging"
 ) {
-  app.use(express.static("./client/build"));
+  app.use(express.static("client/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname + "./client/build/index.html"));
+    res.sendFile(path.join(__dirname + "client/build/index.html"));
   });
 }
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
